bd3be66d45e1580e9556a9011100f3e54601552f
hamad <hamad@ABDULLAHBAKR>
2024-10-22 16:04:05 +0300

    Cache Service.(01)

---
+++ Project.Core/Services.Contract/ICasheService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Core.Services.Contract
+{
+    public interface ICasheService
+    {
+        Task SetCasheKeyAsync(string key, object rasponse, TimeSpan expireTime);
+        Task<string> GetCasheKeyAsync (string key);
+    }
+}
---
+++ Project.Service/Services/Cashes/CasheService.cs
@@ -0,0 +1,34 @@
+﻿using Azure;
+using Project.Core.Services.Contract;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Project.Service.Services.Cashes
+{
+    public class CasheService : ICasheService
+    {
+        private readonly IDatabase _database;
+        public CasheService(IConnectionMultiplexer redis)
+        {
+            _database= redis.GetDatabase();
+        }
+        public async Task<string> GetCasheKeyAsync(string key)
+        {
+            var casheResponse = await _database.StringGetAsync(key);
+            if(casheResponse.IsNullOrEmpty)return null;
+            return casheResponse.ToString();
+        }
+
+        public async Task SetCasheKeyAsync(string key, object rasponse, TimeSpan expireTime)
+        {
+            if(rasponse is null) return;
+            var options = new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+            await _database.StringSetAsync(key,JsonSerializer.Serialize(rasponse,options),expireTime);
+        }
+    }
+}
