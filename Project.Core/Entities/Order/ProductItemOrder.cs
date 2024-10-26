a214e026a4c1988401fbb1ad1e19f9cbe5c13d8a
hamad <hamad@ABDULLAHBAKR>
2024-10-26 22:35:43 +0300

    Order Module - DAL.(14)

---
+++ Project.Core/Entities/Order/ProductItemOrder.cs
@@ -0,0 +1,9 @@
+﻿namespace Project.Core.Entities.Order
+{
+    public class ProductItemOrder
+    {
+        public int ProductId { get; set; }
+        public int ProductName { get; set; }
+        public int PictureUrl { get; set; }
+    }
+}
